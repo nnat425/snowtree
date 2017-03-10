4e14ee407fabc90b9866b67c488fb97f7c7e2c31
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-09 23:05:49 -0500

    secure params and allow creation of new admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,13 +4,23 @@ class AdminsController < ApplicationController
   end
 
   def new
+    @admin = Admin.new
   end
 
   def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      session[:admin_id] = @admin.id
+      redirect_to admins_path(@admin)
+    else
+      @errors = @admin.errors.full_messages
+      render 'new'
+    end
   end
 
   private
 
   def admin_params
+    params.require(:admin).permit(:email,:password)
   end
 end
