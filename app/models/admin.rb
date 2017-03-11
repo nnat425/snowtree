baa1a25059fa8df92f3bac5370de5907b337e130
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-11 17:51:25 -0500

    add validations to admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,11 @@
 class Admin < ApplicationRecord
   has_secure_password
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  validates_format_of :email, with: EMAIL_REGEX, on: :create
+  validates_length_of :password, in: 6..20, on: :create
+
+  validates_presence_of :email
+  validates_presence_of :password
 end
