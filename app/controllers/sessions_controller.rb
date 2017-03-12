1051f4b07ff0b52cfd286b02f20568ba72c76bed
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-12 18:43:09 -0400

    add methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SesssionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @admin = Admin.find_by_email(params[:session][:email])
+    if @admin && @user.authenticate(params[:session][:password])
+      session[:admin_id] = @admin.id
+      redirect_to admins_path(@admin)
+    else
+      @errors = @admin.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def destory
+    session.clear
+    redirect_to root_path
+  end
 end
