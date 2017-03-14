0e540c555773a6b2ce258f32842652faa185e7d6
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-13 21:53:29 -0400

    fix route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get 'admin_panel', to: 'admins#admin_panel'
-  
+  get "sessions/login", to: "sessions#login"
   resources :admins
   resources :sessions
+
+  
 end
