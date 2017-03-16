8c1bf1f0a5a562432eb1f887a9699c7ea260d0d3
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-15 22:02:10 -0400

    add validations to inventory model

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,8 @@
 class Inventory < ApplicationRecord
+  validates_presence_of :type
+  validates_presence_of :material_name
+  validates_presence_of :grade_number
+  validates_presence_of :form
+  validates_presence_of :color
+  validates_presence_of :quantity
 end
