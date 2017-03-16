cf3ab812a87f21c0c295a94d2e6bc36ce184cebc
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-15 22:26:34 -0400

    create inventories controller

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -4,19 +4,30 @@ class InventoriesController < ApplicationController
   end
 
   def new
+    @inventory = Inventory.new
   end
 
   def show
   end
 
   def create
+    @inventory = Inventory.new(inventory_params)
+    if @inventory.save
+      redirect_to inventories_path
+    else
+      @errors = @inventory.errors.full_messages
+      render "new"
+    end
   end
 
   def destroy
+    @inventory.clear
+    redirect_to inventories_path
   end
 
   private
 
   def inventory_params
+    params.require(:inventory).permit(:type, :material_name, :grade_number, :form, :color, :quantity)
   end
 end
