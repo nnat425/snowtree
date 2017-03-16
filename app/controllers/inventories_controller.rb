35946e5d0807a189bdc0b6ae2fadc644a9659fbc
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-15 22:22:02 -0400

    create template methods

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,22 @@
 class InventoriesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def inventory_params
+  end
 end
