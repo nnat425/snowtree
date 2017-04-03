0de7e5108241d06100e84175d992faf76be29ccd
Arshdeep Singh <singh_arsy@yahoo.com>
2017-04-03 11:39:23 -0400

    add image validations to admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -8,6 +8,9 @@ class Admin < ApplicationRecord
   validates_presence_of :email
   validates_presence_of :password
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   def deliver_password_reset_instructions
     self.perishable_token = SecureRandom.hex(4)
     save(validate: false)
