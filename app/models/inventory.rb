4ed6e6c23d7406f6637055882054197d0495b484
Arshdeep Singh <singh_arsy@yahoo.com>
2017-04-06 22:19:49 -0400

    add validations for images

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -7,6 +7,6 @@ class Inventory < ApplicationRecord
   validates_presence_of :quantity
   validates_presence_of :image
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>",large: "500x500>" }, default_url: "missing"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>",large: "500x500>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
