2368c422d5ab1b85e34ada733fa0bd1292806161
Navraj Nat <nnat425@gmail.com>
2017-04-19 22:13:08 -0400

    edit & update working

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -26,7 +26,7 @@ class InventoriesController < ApplicationController
   end
 
   def edit 
-  @inventory = Inventory.find(param[:id])
+  @inventory = Inventory.find(params[:id])
   end
 
   def update
