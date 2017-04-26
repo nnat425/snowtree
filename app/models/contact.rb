d4d2068acbc6ef2fde1361101e24d6975dc8f766
Arshdeep Singh <singh_arsy@yahoo.com>
2017-04-26 14:52:23 -0400

    rename subject

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,13 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
   attribute :number,      :validate => true
+  attribute :message
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "SnowTree Product",
+      :subject => "Customer Contact",
       :to => "snowtree.testing@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
