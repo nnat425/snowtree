0165b072d66e848c26299850e10182e6f662d920
Arshdeep Singh <singh_arsy@yahoo.com>
2017-04-27 09:51:19 -0400

    add snowtree gmail account

--- app/mailers/password_reset_notifier.rb
+++ app/mailers/password_reset_notifier.rb
@@ -3,6 +3,6 @@ class PasswordResetNotifier < ActionMailer::Base
   layout 'mailer'
   def password_reset_instructions(admin)
     @url  = edit_password_reset_url(token: admin.perishable_token)
-    mail(to: admin.email, subject: "Password Reset Instructions")
+    mail(to: "snowtree.testing@gmail.com", subject: "Password Reset Instructions")
   end
 end
