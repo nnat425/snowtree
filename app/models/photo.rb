89cdb7514c2a8782f3a6469977f1a69d4039904e
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-03 20:14:53 -0400

    change photo size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
 
-	has_attached_file :image, :styles => { :medium => "150x150>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "160x160>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 	belongs_to :inventory
 
