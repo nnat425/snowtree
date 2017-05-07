86891a75ceb83c4928b00fd366da6f61d32cf568
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-06 23:14:18 -0400

    create first box information react component

---
+++ app/assets/javascripts/components/home/first_box_information.js.jsx
@@ -0,0 +1,28 @@
+class FirstBoxInformation extends React.Component {
+  render() {
+    return(
+      <div className="container">
+  			<div className="row first-box-padding">
+  				<div className="col-lg-12">
+  					<h2 className="home-title">
+  					<p className="typewrite" data-period="2000" data-type='[ "About SnowTree... "]'>
+  					<span className="wrap"></span></p></h2>
+  				</div>
+  				<div className="col-md-6">
+  					<p>Snowtree environmental solutions is a consulting, technology,, sales, and distribution company Focused on plastic, paper, and metals. With 30 years of trading and technical sales experience , we can provide the information and services to make it possible to save our customers money while getting the best price for their materials.</p>
+  					<ul>
+  						<li>jQuery v1.11.1</li>
+  						<li>Font Awesome v4.2.0</li>
+  						<li>Working PHP contact form with validation</li>
+  						<li>Unstyled page elements for easy customization</li>
+  						<li>17 HTML pages</li>
+  					</ul>
+  				</div>
+  				<div className="col-md-6">
+  					<img className="img-responsive" src="/plastic.jpg" alt="SnowTree"/>
+  				</div>
+  			</div>
+  		</div>
+    )
+  }
+}
