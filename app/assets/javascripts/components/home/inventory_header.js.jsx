b311e3174fefa4e021406be4bc31b6c7a6828e91
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-06 23:20:00 -0400

    react inventory header

---
+++ app/assets/javascripts/components/home/inventory_header.js.jsx
@@ -0,0 +1,11 @@
+class InventoryHeader extends React.Component {
+  render() {
+    return(
+      <div className="col-lg-12">
+        <h2 className="home-title">
+          <p className="typewrite" data-period="2000" data-type='["Checkout Our Inventory"]'>
+          <span className="wrap"></span></p></h2>
+        </div>
+    );
+  }
+}
