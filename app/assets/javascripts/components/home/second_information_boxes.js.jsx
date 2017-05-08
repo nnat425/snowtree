9171644fed6874dd86867aa7160c5bb2102e4fe1
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-08 16:18:18 -0400

    make information boxes same size

--- app/assets/javascripts/components/home/second_information_boxes.js.jsx
+++ app/assets/javascripts/components/home/second_information_boxes.js.jsx
@@ -14,10 +14,10 @@ class SecondInformationBoxes extends React.Component {
   			</div>
   			<div className="col-md-4">
   				<div className="panel">
-  					<div className="panel-heading panel-color1">
+  					<div className="panel-heading panel-color ">
   						<h4><i className="fa fa-fw fa-gift"></i> Consultation</h4>
   					</div>
-  					<div className="panel-body panel-color-body ">
+  					<div className="panel-body panel-color-body consulation ">
   						<p>We will match up the supply of material with the correct customer to achieve a best net back to the supplier. We make sure that our suppliers produce quality material for customers around the world.  The ability technically assures both the customer and the supplier of smooth transaction with rarely issues of quality.  </p>
   					</div>
   				</div>
@@ -27,7 +27,7 @@ class SecondInformationBoxes extends React.Component {
   					<div className="panel-heading panel-color2">
   						<h4><i className="fa fa-fw fa-compass"></i> Inventory</h4>
   					</div>
-  					<div className="panel-body panel-color-body ">
+  					<div className="panel-body panel-color-body inventory">
   						<p>Our knowledge of plastics and paper allows us to handle mixed loads that other companies can't. We will research and test your products to get you the best possibilities of selling or purchasing with confidence and security that our client require.</p>
   					</div>
   				</div>
