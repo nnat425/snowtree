f270c07871e14e5fd138d86680d621ffcd7706f2
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-08 22:38:47 -0400

    change up boxes color and style

--- app/assets/javascripts/components/home/second_information_boxes.js.jsx
+++ app/assets/javascripts/components/home/second_information_boxes.js.jsx
@@ -2,35 +2,40 @@ class SecondInformationBoxes extends React.Component {
   render(){
     return(
       <div className="row information-boxes">
-  			<div className="col-md-4 ">
-  				<div className="panel">
-  					<div className="panel-heading panel-color">
-  						<h4 className="font-style-header"><i className="glyphicon glyphicon-globe"></i> Services</h4>
-  					</div>
-  					<div className="panel-body panel-color-body service-box ">
-  						<p className="font-style-info">Our capabilities include laboratories testing, material identification, technical assistance on material selection for packaging and recyclability. </p>
+  			<div className="col-lg-4 ">
+          <div className= "info-text">
+          <i className="glyphicon glyphicon-globe"></i>
+          <h4 className="font-style-header"> Services</h4>
+  				<div className="">
+  					<div className="  ">
+      			<p className="font-style-info">Our capabilities include laboratories testing, material identification, technical assistance on material selection for packaging and recyclability. </p>
   					</div>
   				</div>
   			</div>
-  			<div className="col-md-4">
-  				<div className="panel">
-  					<div className="panel-heading panel-color ">
-  						<h4 className="font-style-header"><i className="glyphicon glyphicon-bookmark"></i> Consultation</h4>
-  					</div>
-  					<div className="panel-body panel-color-body consulation ">
+      </div>
+  			<div className="col-lg-4">
+          <div className= "info-text">
+          <i className="glyphicon glyphicon-bookmark"></i>
+          <h4 className="font-style-header"> Consultation</h4>
+  				<div className="">
+  					<div className=" consulation ">
   						<p className="font-style-info">We will match up the supply of material with the correct customer to achieve a best net back to the supplier. We make sure that our suppliers produce quality material for customers around the world.  </p>
   					</div>
   				</div>
+        </div>
   			</div>
-  			<div className="col-md-4">
-  				<div className="panel">
-  					<div className="panel-heading panel-color2">
-  						<h4 className="font-style-header"><i className="glyphicon glyphicon-shopping-cart"></i> Inventory</h4>
+  			<div className="col-lg-4">
+            <div className= "info-text">
+          <i className="glyphicon glyphicon-shopping-cart"></i>
+        <h4 className="font-style-header">Inventory</h4>
+  				<div className="">
+  					<div className="">
   					</div>
-  					<div className="panel-body panel-color-body inventory">
+  					<div className=" inventory">
   						<p className="font-style-info">Our knowledge of plastics and paper allows us to handle mixed loads that other companies can't. We will research and test your products to get you the best possibilities of selling or purchasing with confidence and security that our client require.</p>
   					</div>
   				</div>
+          </div>
   			</div>
       </div>
     );
