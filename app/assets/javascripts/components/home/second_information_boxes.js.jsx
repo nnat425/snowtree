626f92b6b2a3b2cd7cfb471d7f50d8c8919d22e3
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-08 22:45:08 -0400

    fix glyiphicon error

--- app/assets/javascripts/components/home/second_information_boxes.js.jsx
+++ app/assets/javascripts/components/home/second_information_boxes.js.jsx
@@ -4,40 +4,28 @@ class SecondInformationBoxes extends React.Component {
       <div className="row information-boxes">
   			<div className="col-lg-4 ">
           <div className= "info-text">
-          <i className="glyphicon glyphicon-globe"></i>
-          <h4 className="font-style-header"> Services</h4>
-  				<div className="">
-  					<div className="  ">
+            <i className="glyphicon glyphicon-globe"></i>
+            <h4 className="font-style-header"> Services</h4>
       			<p className="font-style-info">Our capabilities include laboratories testing, material identification, technical assistance on material selection for packaging and recyclability. </p>
-  					</div>
-  				</div>
-  			</div>
-      </div>
+  			  </div>
+        </div>
   			<div className="col-lg-4">
           <div className= "info-text">
-          <i className="glyphicon glyphicon-bookmark"></i>
-          <h4 className="font-style-header"> Consultation</h4>
-  				<div className="">
-  					<div className=" consulation ">
-  						<p className="font-style-info">We will match up the supply of material with the correct customer to achieve a best net back to the supplier. We make sure that our suppliers produce quality material for customers around the world.  </p>
-  					</div>
-  				</div>
-        </div>
+            <i className="glyphicon glyphicon-bookmark"></i>
+            <h4 className="font-style-header"> Consultation</h4>
+  					<p className="font-style-info">We will match up the supply of material with the correct customer to achieve a best net back to the supplier. We make sure that our suppliers produce quality material for customers around the world.  </p>
+          </div>
   			</div>
   			<div className="col-lg-4">
-            <div className= "info-text">
-          <i className="glyphicon glyphicon-shopping-cart"></i>
-        <h4 className="font-style-header">Inventory</h4>
-  				<div className="">
-  					<div className="">
-  					</div>
+          <div className= "info-text">
+            <i className="glyphicon glyphicon-shopping-cart"></i>
+            <h4 className="font-style-header">Inventory</h4>
   					<div className=" inventory">
   						<p className="font-style-info">Our knowledge of plastics and paper allows us to handle mixed loads that other companies can't. We will research and test your products to get you the best possibilities of selling or purchasing with confidence and security that our client require.</p>
-  					</div>
-  				</div>
           </div>
   			</div>
       </div>
+    </div>
     );
   }
 }
