abc492a0ff1b17408838bcb3ebd696e6d1a89900
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-10 17:36:23 -0400

    reduce about snowtree paragraph

--- app/assets/javascripts/components/home/first_box_information.js.jsx
+++ app/assets/javascripts/components/home/first_box_information.js.jsx
@@ -10,7 +10,7 @@ class FirstBoxInformation extends React.Component {
             	<h2 className="about-title">About SnowTree</h2>
   					<p className="about-info">Snowtree environmental solutions is a consulting, technology, sales, and distribution company Focused on plastic, paper, and metals.</p>
             <p className="about-info">With 30 years of trading and technical sales experience , we can provide the information and services to make it possible to save our customers money while getting the best price for their materials.</p>
-              <p className="about-info">We supply in  black virgin color concentrate for pe, pp, ps, and Eva. We can also provide Virgin plastic resins such as prime virgin, generic, prime, wide spec, off grade and surplus lots. In addition Polyethylene and polypropylene grinding from HMW pipe to crates. We also have the ability to wash and grind most PP and PE at our Ohio plant.</p>
+              <p className="about-info">We supply in  black virgin color concentrate, Virgin plastic resins along with Polyethylene and polypropylene grinding from HMW pipe to crates. We also have the ability to wash and grind most PP and PE at our Ohio plant.</p>
   				</div>
   			</div>
   		</div>
