3deecc08f663d9498b19547f408bbc2d575b7072
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-10 09:54:29 -0400

    fix spacing

--- app/assets/javascripts/components/home/first_box_information.js.jsx
+++ app/assets/javascripts/components/home/first_box_information.js.jsx
@@ -10,7 +10,6 @@ class FirstBoxInformation extends React.Component {
             	<h2 className="about-title">About SnowTree</h2>
   					<p className="about-info">Snowtree environmental solutions is a consulting, technology, sales, and distribution company Focused on plastic, paper, and metals.</p>
             <p className="about-info">With 30 years of trading and technical sales experience , we can provide the information and services to make it possible to save our customers money while getting the best price for their materials.</p>
-
   				</div>
   			</div>
   		</div>
