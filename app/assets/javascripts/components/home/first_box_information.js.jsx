0978a2b6f3f0ccaeb04ec98693186fcc5e22a6fe
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-10 09:50:16 -0400

    change paragraph styles

--- app/assets/javascripts/components/home/first_box_information.js.jsx
+++ app/assets/javascripts/components/home/first_box_information.js.jsx
@@ -3,15 +3,14 @@ class FirstBoxInformation extends React.Component {
     return(
       <div className="container">
   			<div className="row first-box-padding">
-  				<div className="col-lg-12">
+          <div className="col-md-6">
+  					<img className="img-responsive" src="/plastic.jpg" alt="SnowTree"/>
   				</div>
   				<div className="col-md-6">
             	<h2 className="about-title">About SnowTree</h2>
-  					<p className="about-info">Snowtree environmental solutions is a consulting, technology,, sales, and distribution company Focused on plastic, paper, and metals. With 30 years of trading and technical sales experience , we can provide the information and services to make it possible to save our customers money while getting the best price for their materials.</p>
-  			
-  				</div>
-  				<div className="col-md-6">
-  					<img className="img-responsive" src="/plastic.jpg" alt="SnowTree"/>
+  					<p className="about-info">Snowtree environmental solutions is a consulting, technology, sales, and distribution company Focused on plastic, paper, and metals.</p>
+            <p className="about-info">With 30 years of trading and technical sales experience , we can provide the information and services to make it possible to save our customers money while getting the best price for their materials.</p>
+
   				</div>
   			</div>
   		</div>
