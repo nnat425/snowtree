7de790011c4e154c6a2e86d5d15a74f37344b8eb
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-10 21:55:45 -0400

    change header style

--- app/assets/javascripts/components/home/inventory_header.js.jsx
+++ app/assets/javascripts/components/home/inventory_header.js.jsx
@@ -2,7 +2,7 @@ class InventoryHeader extends React.Component {
   render() {
     return(
       <div className="col-lg-12">
-        <u><h2 className="home-title">Checkout Our Inventory</h2></u>
+      <h2 className="home-title">Checkout Our Inventory</h2>
       </div>
     );
   }
