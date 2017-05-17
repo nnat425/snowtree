03fb1b79bfc5f934d37d9357f789b60b6de2bbfc
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-17 12:15:13 -0400

    add react to second div

---
+++ app/assets/javascripts/components/consulting/second-div.js.jsx
@@ -0,0 +1,17 @@
+class SecondDiv extends React.Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-8">
+            <h3>A Better Education center</h3>
+            <p>
+              Thousands of teenagers across the UK will have school lessons in mindfulness in an experiment designed to see if it can protect against mental illness.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
