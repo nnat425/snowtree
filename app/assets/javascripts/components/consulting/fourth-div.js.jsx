dc4ae910fc3cdac3ebabe635fda297df8fcf626b
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-17 12:51:46 -0400

    add react to 4th div

--- app/assets/javascripts/components/consulting/fourth-div.js.jsx
+++ app/assets/javascripts/components/consulting/fourth-div.js.jsx
@@ -1,4 +1,4 @@
-class FirstDiv extends React.Component {
+class FourthDiv extends React.Component {
   render() {
     return(
         <div className="container">
@@ -8,10 +8,13 @@ class FirstDiv extends React.Component {
                 <h1 className="tagline header-text-size">Consulting</h1>
                 <p>ntroduce the visitor to the business using clear, informative text. </p>
                 <p>Use well-targeted keywords within your sentences to make sure search engines can find the business.</p>
+                <br/>
+                <a className="btn btn-success btn-md" href="#">Call to Action »</a>
               </div>
             </div>
           </div>
         </div>
-    );
+
+      );
+    }
   }
-}
