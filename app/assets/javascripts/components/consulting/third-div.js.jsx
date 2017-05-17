a7cd66fb2b9e30b1e2c2d3c67a0aea636752e50b
Arshdeep Singh <singh_arsy@yahoo.com>
2017-05-17 12:19:14 -0400

    add react to third div

---
+++ app/assets/javascripts/components/consulting/third-div.js.jsx
@@ -0,0 +1,37 @@
+class ThirdDiv extends React.Component {
+  render() {
+    return(
+      <div className="row">
+       <div className="col-md-16">
+         <div className="text-center">
+           <h3 className="text-center header-third ">Lorem ipsum dolor sit amet consectetur.</h3>
+           <hr className=" container style12"/>
+           <p className="text-center infor-consol">The small element is used to create a lighter, secondary text in any heading:</p>
+         </div>
+         <div className="row info-consol-sub">
+           <div className="col-md-3 text-center ">
+             <div className="icon-wrapper"><i className="fa fa-users custom-icon fa-5x"></i></div>
+               <h4 className= "sub-divider-header">Intensive Practical Classes</h4>
+               <p className="text-center infor-consol">The small element is used to create a lighter, secondary text in any heading:</p>
+           </div>
+           <div className="col-md-3 text-center ">
+             <div className="icon-wrapper"><i className="fa fa-clipboard custom-icon fa-5x"></i></div>
+               <h4  className= "sub-divider-header">Intensive Practical Classes</h4>
+               <p className="text-center infor-consol">The small element is used to create a lighter, secondary text in any heading:</p>
+           </div>
+           <div className="col-md-3 text-center ">
+             <div className="icon-wrapper"><i className="fa fa-bar-chart custom-icon fa-5x"></i></div>
+               <h4  className= "sub-divider-header">Intensive Practical Classes</h4>
+               <p className="text-center infor-consol">The small element is used to create a lighter, secondary text in any heading:</p>
+           </div>
+           <div className="col-md-3 text-center ">
+             <div className="icon-wrapper"><i className="fa fa-cubes custom-icon fa-5x"></i></div>
+               <h4 className= "sub-divider-header">Intensive Practical Classes</h4>
+               <p className="text-center infor-consol">The small element is used to create a lighter, secondary text in any heading:</p>
+             </div>
+           </div>
+         </div>
+       </div>
+    );
+  }
+}
