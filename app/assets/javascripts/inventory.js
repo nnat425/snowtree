5f719febacee84f9313c069c8dbaa3d1c7c304ff
Arshdeep Singh <singh_arsy@yahoo.com>
2017-06-14 10:39:36 -0400

    add view for images

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -41,3 +41,25 @@ $(document).ready(function(){
     console.log(files);
   })
 })
+
+
+//show page
+
+$(document).ready(function(){
+$('.carousel[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+  	}
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+});
